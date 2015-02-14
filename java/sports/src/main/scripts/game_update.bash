6e903d57157ce4be2a10e6584e949a9b6fe63ac1
Joe Conley <josephpconley@gmail.com>
2015-02-14 15:44:22 -0500

    more cleanup

--- java/sports/src/main/scripts/game_update.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# dos2unix this file on itself once done editing to make readable to crontab
-#
-PATH=$PATH:/cygdrive/c/ant/bin;
-export PATH
-
-cd /cygdrive/c/tomcat/webapps/sports/scripts
-ant -buildfile game_update.xml -verbose >> updates.log
