a133ad275ecc1f5fb7841dad2690507017219d6f
Joe Conley <josephpconley@gmail.com>
2015-03-31 09:35:22 -0400

    more ES changes

--- scala/build.sbt
+++ scala/build.sbt
@@ -11,5 +11,9 @@ libraryDependencies ++= Seq(
   "org.scala-lang" % "scala-swing" % "2.10.4",
   "com.sksamuel.elastic4s" % "elastic4s_2.11" % "1.3.4",
   "com.typesafe.play" %% "play-json" % "2.3.4",
-  "org.specs2" %% "specs2-core" % "2.4.9" % "test"
+  "org.specs2" %% "specs2-core" % "2.4.9" % "test",
+  "com.typesafe.slick" %% "slick" % "3.0.0-RC1",
+  "org.slf4j" % "slf4j-nop" % "1.6.4",
+  "com.h2database" % "h2" % "1.3.175",
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 )
--- scala/src/main/scala/elasticsearch/ES.scala
+++ scala/src/main/scala/elasticsearch/ES.scala
@@ -1,10 +1,11 @@
 package elasticsearch
 
-import com.sksamuel.elastic4s.ElasticDsl._
 import com.sksamuel.elastic4s._
+import com.sksamuel.elastic4s.ElasticDsl._
 import org.elasticsearch.client.transport.TransportClient
 import org.elasticsearch.common.settings.{ImmutableSettings, Settings}
 import org.elasticsearch.common.transport.InetSocketTransportAddress
+import play.api.libs.json.Json
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.Await
@@ -29,15 +30,13 @@ object ES extends App with ESClient {
   val filterList = new ListBuffer[FilterDefinition]
 
   filterList += termFilter("listingId", 1)
-//
+
 //  val categories: Seq[Long] = Seq(78, 107)
 //  filterList += termsFilter("categories.id", categories:_*) execution "and"
 //
 //  filterList += existsFilter("reviews")
 
 //  filterList += existsFilter("offers")
-
-//    filterList += queryFilter(stringQuery("for") field "offers.description")
 
 //  filterList += queryFilter(stringQuery("offer") field "offers.title")
 
@@ -46,19 +45,26 @@ object ES extends App with ESClient {
 //  val keywordFields = Seq("description", "servicesDescription", "reviews.text", "offers.title", "offers.description")
 //  filterList += queryFilter(multiMatchQuery("door") fields keywordFields)
   
-//  filterList += (rangeFilter("recentGrade") gte "3")
+//  filterList += (rangeFilter("recentGrade") gte "C")
+
+//  val marketPostalCode = "80005"
+//  filterList += or(termFilter("postalCodesServiced", marketPostalCode), termFilter("postalCodesServiced", marketPostalCode))
+
 //
 //  filterList += regexFilter("contactInfo.phoneNumber", "30344.*")
+//  filterList += termFilter("contactInfo.phoneNumber", "30344")
 
   val filter = filteredQuery filter bool {
     must(filterList.toList:_*)
   }
-  val query = search in "search_v1" -> "serviceProvider" query filter start 0 limit 5
+
+  val sorts = by field "nameKeyword"
+
+  val query = search in "search_v1" -> "serviceProvider" query filter sort sorts start 0 limit 50
   val res = Await.result(es.execute(query), Duration.Inf)
 
-  println(res)
-
-  res.getHits.hits().toIterable.foreach { h =>
-    println(h.getSource)
+  println(res.getHits.getTotalHits)
+  res.getHits.hits().foreach { h =>
+    println(h.getSourceAsString)
   }
 }
